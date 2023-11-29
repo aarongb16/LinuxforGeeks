6d19194ae3a23a676995fcfd2f81ea7e043f04de
Aapohmoses <78748017+aarongb16@users.noreply.github.com>
2023-11-29 08:26:14 -0400

    Add files via upload

---
+++ docs/archivos/js/script.js
@@ -0,0 +1,9 @@
+function aceptarCookies() {
+    // Aquí puedes agregar el código para almacenar la aceptación de cookies, por ejemplo, utilizando el almacenamiento local del navegador
+    document.getElementById('cookie-banner').style.display = 'none';
+  }
+  
+  function rechazarCookies() {
+    // Aquí puedes agregar el código para rechazar el uso de cookies, por ejemplo, eliminando cualquier cookie almacenada
+    document.getElementById('cookie-banner').style.display = 'none';
+  }
